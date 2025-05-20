f50fc6f743bce717fd4e585ece6f133e521c30c2
PicositaPosting <picositaposting477@gmail.com>
2025-05-19 20:10:22 -0400

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
-const adsterraLink = "https://www.profitableratecpm.com/tcsm3yz6?key=e3bef76eb73c7c8e0feda2955b68aae1";
+const adsterraLink = "https://vt.tiktok.com/ZShgBPF1c/";
 
 function abrirDosVeces(event) {
     if (event.target.classList.contains("boton-telegram")) {
