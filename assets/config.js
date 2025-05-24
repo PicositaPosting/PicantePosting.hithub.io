bf0ec8130558862c3e701c9a1105dae2ee868294
PicositaPosting <picositaposting477@gmail.com>
2025-05-24 11:06:29 -0400

    ajustes en config.js

--- assets/config.js
+++ assets/config.js
@@ -1,11 +1,16 @@
+// config.js
 const adsterraLink = "https://www.profitableratecpm.com/tcsm3yz6?key=e3bef76eb73c7c8e0feda2955b68aae1";
+const directLink2 = "https://www.youtube.com/";
 
+// Listener global original (que abría solo adsterraLink) – que intentaremos remover en el HTML:
 function abrirDosVeces(event) {
-    if (event.target.classList.contains("boton-telegram")) {
-        return;
-    }
-    window.open(adsterraLink, "_blank");
-    window.open(adsterraLink, "_blank");
+  if (
+    event.target.classList.contains("boton-telegram") ||
+    event.target.classList.contains("boton-mas-videos")
+  ) {
+    return;
+  }
+  window.open(adsterraLink, "_blank");
+  window.open(adsterraLink, "_blank");
 }
-
 document.addEventListener("click", abrirDosVeces);
