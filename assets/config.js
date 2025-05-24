61e5833984671973a99e822b61b096cc823a7838
PicositaPosting <picositaposting477@gmail.com>
2025-05-24 14:29:07 -0400

    ajuste

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,6 @@
 // config.js
 const adsterraLink = "https://www.profitableratecpm.com/tcsm3yz6?key=e3bef76eb73c7c8e0feda2955b68aae1";
+const directLink1 = "https://www.youtube.com/";
 const directLink2 = "https://www.profitableratecpm.com/m2cvdnbxc6?key=c5db2b4842b3332c507fd71ef549a478";
 
 // Listener global original (que abría solo adsterraLink) – que intentaremos remover en el HTML:
