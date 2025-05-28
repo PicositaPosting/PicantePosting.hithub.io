b4d7400a63a576d559af8577eab05ab38790e502
PicositaPosting <picositaposting477@gmail.com>
2025-05-28 12:20:04 -0400

    directLink2

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 // config.js
 const adsterraLink = "https://www.profitableratecpm.com/tcsm3yz6?key=e3bef76eb73c7c8e0feda2955b68aae1";
 const directLink1 = "https://www.profitableratecpm.com/tcsm3yz6?key=e3bef76eb73c7c8e0feda2955b68aae1";
-const directLink2 = "https://www.profitableratecpm.com/m2cvdnbxc6?key=c5db2b4842b3332c507fd71ef549a478";
+const directLink2 = "https://deputydelicacyopt.com/ik66puu2i?key=80c2d84b0a5a536c95002e03419d177d";
 
 // Listener global original (que abría solo adsterraLink) – que intentaremos remover en el HTML:
 function abrirDosVeces(event) {
