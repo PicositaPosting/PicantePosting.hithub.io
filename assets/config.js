9a45522c4b782596e45ab2d591429d81cbe8f4e6
PicositaPosting <picositaposting477@gmail.com>
2025-05-30 10:45:10 -0400

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,8 @@
 // config.js
 const adsterraLink = "https://www.profitableratecpm.com/tcsm3yz6?key=e3bef76eb73c7c8e0feda2955b68aae1";
 const directLink1 = "https://www.profitableratecpm.com/tcsm3yz6?key=e3bef76eb73c7c8e0feda2955b68aae1";
-const directLink2 = "https://deputydelicacyopt.com/ik66puu2i?key=80c2d84b0a5a536c95002e03419d177d";
+const directLink2 = "https://t.me/picantecinedecalidad";
+const directLink4 = "https://deputydelicacyopt.com/ik66puu2i?key=80c2d84b0a5a536c95002e03419d177d";
 const directLink3 = "https://deputydelicacyopt.com/ptudbe5hbj?key=4c071702d0a6c663149b7b5da3601a7a";
 
 // assets/config.js
