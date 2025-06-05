c53e0dce0dc8426dcacf329098e436110f0f2fac
PicositaPosting <picositaposting477@gmail.com>
2025-06-05 14:02:29 -0400

    radd

--- assets/config.js
+++ assets/config.js
@@ -4,7 +4,7 @@ const directLink1 = "https://www.profitableratecpm.com/tcsm3yz6?key=e3bef76eb73c
 const directLink2 = "https://deputydelicacyopt.com/ik66puu2i?key=80c2d84b0a5a536c95002e03419d177d";
 const directLink3 = "https://www.profitableratecpm.com/rjq8v2401f?key=d4f8d4d0f7f53da6e37e7d66dc1709e0";
 const directLink4 = "https://www.profitableratecpm.com/m4ssfi2qbr?key=5a95789e92daacb803d6078b2b97ce8a";
-const directLink5 = "";
+const directLink5 = "https://www.profitableratecpm.com/g2b6z4fgv?key=c908dfa9008818de2fcaff44dc7af342";
 const directLink6 = "";
 const directLink7 = "";
 
