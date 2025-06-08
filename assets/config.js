584129f70a1df9ea2bcea437fc4cbc310faa81ba
PicositaPosting <picositaposting477@gmail.com>
2025-06-08 10:28:06 -0400

    add

--- assets/config.js
+++ assets/config.js
@@ -6,7 +6,12 @@ const directLink3 = "https://www.profitableratecpm.com/rjq8v2401f?key=d4f8d4d0f7
 const directLink4 = "https://www.profitableratecpm.com/m4ssfi2qbr?key=5a95789e92daacb803d6078b2b97ce8a";
 const directLink5 = "https://www.profitableratecpm.com/g2b6z4fgv?key=c908dfa9008818de2fcaff44dc7af342";
 const directLink6 = "https://www.profitableratecpm.com/ua1h6q93?key=d581323fa27c963488ab314c39fd4950";
-const directLink7 = "";
+const directLink7 = "https://www.profitableratecpm.com/jmj3wkux?key=a8c86769b4c352da2cb2518bc9627747";
+const directLink8 = "https://www.profitableratecpm.com/hu01rghyp?key=027256910ee7e41477c64b8471ef5840";
+const directLink9 = "https://www.profitableratecpm.com/jwegu28mzv?key=9a8b8c091c7f5d5a4a8013ee7127552e";
+const directLink10 = "";
+const directLink11 = "";
+const directLink12 = "";
 
 
 // assets/config.js
